8ab33aed3fa473ccd3b1217f3686a98fbc313b6e
nansu0425 <nansu0425@gmail.com>
2026-05-21 10:07:24 +0900

    feat: 프로그래머스 42577 3회차 풀이
    - 문제 이름: 전화번호 목록
    - 남은 시간: 7/ 30 (성공, 솔루션 2개 포함 기준 시간)

--- Programmers/Lv2/42577.cpp
+++ Programmers/Lv2/42577.cpp
@@ -1,44 +1,74 @@
 #include <string>
 #include <vector>
 #include <unordered_set>
+#include <algorithm>
 
-bool solution(std::vector<std::string> phone_book) {
-    // 1. phone set과 prefix count set을 만든다
-    std::unordered_set<std::string> phoneSet;
-    std::unordered_set<size_t> prefixCountSet;
-    for (const auto& phone : phone_book)
-    {
-        phoneSet.insert(phone);
-        prefixCountSet.insert(phone.size());
-    }
+// hash 풀이
+//bool solution(std::vector<std::string> phone_book) {
+//    // 어떤 번호가 다른 번호의 접두어인 경우 -> false
+//
+//    // 1. 번호 set을 만든다
+//    // 2. prefix len set을 만든다
+//    std::unordered_set<std::string> phoneSet;
+//    std::unordered_set<size_t> prefixLenSet;
+//    for (const std::string& phone : phone_book)
+//    {
+//        phoneSet.insert(phone);
+//        prefixLenSet.insert(phone.size());
+//    }
+//
+//    // 3. 모든 번호 순회
+//    for (const std::string& phone : phone_book)
+//    {
+//        // 4. prefix len set 순회
+//        for (size_t prefixLen : prefixLenSet)
+//        {
+//            // 5. 번호 길이 <= prefix len -> continue
+//            if (phone.size() <= prefixLen)
+//            {
+//                continue;
+//            }
+//
+//            // 6. phone의 prefix len substr이 번호 set에 있으면 false
+//            std::string prefix = phone.substr(0, prefixLen);
+//            if (phoneSet.count(prefix))
+//            {
+//                return false;
+//            }
+//        }
+//    
+//    }
+//
+//    return true;
+//}
 
-    // 2. phone_book을 순회
-    for (const std::string& phone : phone_book)
+// 정렬 + 인접 비교 풀이
+bool solution(std::vector<std::string> phone_book)
+{
+    // 사전순 정렬 -> 어떤 단어의 prefix인 단어가 존재하는 경우, prefix 단어는 그 단어 바로 앞으로 오게 된다
+
+    std::sort(phone_book.begin(), phone_book.end());
+
+    for (size_t i = 1; i < phone_book.size(); ++i)
     {
-        // 3. prefix count set 순회
-        for (size_t prefixCount : prefixCountSet)
+        const std::string& prev = phone_book[i - 1];
+        const std::string& cur = phone_book[i];
+
+        if (cur.compare(0, prev.size(), prev) == 0)
         {
-            if (phone.size() <= prefixCount)
-            {
-                continue;
-            }
-
-            // 4. prefix가 phone set에 있는지 확인
-            const std::string prefix = phone.substr(0, prefixCount);
-            if (phoneSet.count(prefix))
-            {
-                return false;
-            }
+            return false;
         }
     }
-        
+
     return true;
 }
 
 #ifdef LOCAL
 int main()
 {
-    bool answer1 = solution({ "119", "97674223", "1195524421" });
+    bool answer1 = solution({ "119", "97674223", "1195524421" }); // false
+    bool answer2 = solution({ "123","456","789" }); // true
+    bool answer3 = solution({ "12","123","1235","567","88" }); // false
 
     return 0;
 }
